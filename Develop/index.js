777d10df0c3a0ffb08b8a447ba63c1fc2d34e553
Colin Rent <colinrent@Colins-MacBook-Pro.local>
2024-01-31 08:00:51 -0500

    added code

--- Develop/index.js
+++ Develop/index.js
@@ -1,74 +1,123 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateMarkdown = require('./utils/generateMarkdown.js');
+const utilityFunctions = require('./utils/utility-functions.js');
 
-// array of questions for user
-const questions = [
-    {
-        type: 'input',
-        name: 'title',
-        message: 'What is the title of your project?'
-    },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Please provide a description of your project.'
-    },
-    {
-        type: 'input',
-        name: 'installation',
-        message: 'Please provide installation instructions for your project.'
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'Please provide usage information for your project.'
-    },
-    {
-        type: 'input',
-        name: 'contributing',
-        message: 'Please provide contribution guidelines for your project.'
-    },
-    {
-        type: 'input',
-        name: 'tests',
-        message: 'Please provide test instructions for your project.'
-    },
-    {
-        type: 'list',
-        name: 'license',
-        message: 'Please select a license for your project.',
-        choices: ['MIT', 'Apache', 'GPL', 'BSD', 'None']
-    },
-    {
-        type: 'input',
-        name: 'github',
-        message: 'Please provide your GitHub username.'
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'Please provide your email address.'
-    }
-];
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project?',
+            validate: (value) => {
+                if (value) {
+                    return true;
+                } else {
+                    return 'Please enter a title for your project.';
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please provide a description of your project.',
+            validate: (value) => {
+                if (value) {
+                    return true;
+                } else {
+                    return 'Please enter a description for your project.';
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Please provide installation instructions for your project.',
+            validate: (value) => {
+                if (value) {
+                    return true;
+                } else {
+                    return 'Please enter installation instructions for your project.';
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Please provide usage instructions for your project.',
+            validate: (value) => {
+                if (value) {
+                    return true;
+                } else {
+                    return 'Please enter usage instructions for your project.';
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: 'Please provide contribution guidelines for your project.',
+            validate: (value) => {
+                if (value) {
+                    return true;
+                } else {
+                    return 'Please enter contribution guidelines for your project.';
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: 'Please provide test instructions for your project.',
+            validate: (value) => {
+                if (value) {
+                    return true;
+                } else {
+                    return 'Please enter test instructions for your project.';
+                }
+            }
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Please select a license for your project.',
+            choices: ['MIT', 'Apache', 'GPL', 'BSD', 'None'],
+            validate: (value) => {
+                if (value) {
+                    return true;
+                } else {
+                    return 'Please select a license for your project.';
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Please provide your GitHub username.',
+            validate: (value) => {
+                if (value) {
+                    return true;
+                } else {
+                    return 'Please enter your GitHub username.';
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please provide your email address.',
+            validate: (value) => {
+                if (value) {
+                    return true;
+                } else {
+                    return 'Please enter your email address.';
+                }
+            }
+        },
+    ]).then((response) => {
+        const generateMarkdown = utilityFunctions.createMarkdown;
 
-// function to write README file
-
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, (err) =>
-    err ? console.error(err) : console.log('Success! Your README file has been generated!')
-    );
-}
-
-// function to initialize program
-function init() {
-    inquirer.prompt(questions)
-    .then((data) => {
-        writeToFile('README.md', generateMarkdown(data));
+        fs.writeFile(`./created_READMEs/${response.title}-README.md`, generateMarkdown(response), (err) => {
+            if (err) throw err;
+            console.log('The file has been saved!');
+        });
     });
-}
-
-// function call to initialize program
-init(); 
-
-
